53354cc0193232a448330568bebc8af17f20310e
AkonRay <72960660+Lixtic@users.noreply.github.com>
2023-12-26 00:09:02 +0000

    style updated

--- script.js
+++ script.js
@@ -1,80 +1,115 @@
-// Sample lesson data in JSON format
-const lessons =  [
-    {
-      "id": 1,
-      "title": "Introduction to Biology",
-      "content": "Discuss the basics of Biology, its main branches, and their importance.",
-      "date": "2023-04-10",
-      "resources": [
-        {
-          "type": "textbook",
-          "title": "Biology: The Unity and Diversity of Life",
-          "author": "Starr, Taggart, Evers, Starr"
-        },
-        {
-          "type": "website",
-          "url": "https://www.khanacademy.org/science/biology",
-          "description": "Khan Academy Biology Resources"
-        }
-      ]
-    },
-    {
-      "id": 2,
-      "title": "Photosynthesis",
-      "content": "Explore the process plants use to convert sunlight into energy. Cover the chemical equation for photosynthesis and its significance.",
-      "date": "2023-04-17",
-      "resources": [
-        {
-          "type": "video",
-          "url": "https://www.youtube.com/watch?v=o9BqrSAHbTc",
-          "description": "Photosynthesis: Crash Course Biology #8"
-        },
-        {
-          "type": "article",
-          "url": "https://www.nature.com/articles/nature08233",
-          "description": "A new perspective on photosynthesis in research"
-        }
-      ]
-    },
-    {
-      "id": 3,
-      "title": "Human Digestive System",
-      "content": "An overview of the human digestive system, its components, and their functions.",
-      "date": "2023-04-24",
-      "resources": [
-        {
-          "type": "diagram",
-          "url": "https://www.scientificcharts.com/products/human-digestive-system-chart",
-          "description": "Chart of the Human Digestive System"
-        },
-        {
-          "type": "interactive module",
-          "url": "https://www.visiblebody.com/learn/digestive",
-          "description": "Visible Body: Digestive System"
-        }
-      ]
-    }
-  ];
 
-function loadLessons() {
-    const lessonsContainer = document.getElementById('lessons-container');
-    lessonsContainer.innerHTML = ''; // Clear the container
-    lessons.forEach((lesson) => {
-        const lessonElement = document.createElement('div');
-        lessonElement.className = 'lesson';
-        lessonElement.innerHTML = `
-            <h2>${lesson.title}</h2>
-            <p>${lesson.content}</p>
-        `;
-        lessonsContainer.appendChild(lessonElement);
-    });
+        
+// Function to fetch config.json
+async function fetchConfig() {
+    const response = await fetch('config.json');
+    const config = await response.json();
+    return config;
 }
 
-document.getElementById('add-lesson').addEventListener('click', function() {
-    // For simplicity, we'll just log to the console here
-    //  you would present a form to enter new lesson details
-    console.log('Add lesson functionality to be implemented...');
+async function query(data) {
+    const config = await fetchConfig();
+
+    const response = await fetch(
+        config.url2,
+        {
+            method: "POST",
+            headers: { 
+                'Content-Type': 'application/json',
+        'Authorization': config.authorization
+            },
+            
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+    console.log(response);
+}
+
+
+function appendMessage(message, sender) {
+    const chatLog = document.getElementById('chat-log');
+    const messageElement = document.createElement('div');
+    messageElement.classList.add(sender === 'user' ? 'user-message' : 'bot-message');
+    messageElement.innerText = message;
+    chatLog.appendChild(messageElement);
+    chatLog.scrollTop = chatLog.scrollHeight; // Auto-scroll to bottom
+}
+
+
+// Function to generate Teacher activity
+async function generateTeacherActivity() {
+const indicatorValue = document.getElementById('indicatorId').value;
+const selectElement = document.getElementById('genOptions');
+const loader = document.getElementById('loader');
+if (!indicatorValue) return;
+loader.style.display = 'block';
+
+// Handle the change event of the select element
+selectElement.addEventListener('change', async function() {
+const selectedOption = selectElement.options[selectElement.selectedIndex].text;
+
+if (selectedOption === 'Teacher Activities') {
+    const botResponses = await query({ "inputs": "steps to introduce a lesson on how to" + indicatorValue });
+    displayResponse(botResponses, 'phase-duration');
+} else if (selectedOption === 'Learner Activies') {
+    const botResponses = await query({ "inputs": "learner activities on  a lesson on how to " + indicatorValue });
+    displayResponse(botResponses, 'learners-activities');
+} else if (selectedOption === 'Resources') {
+    const botResponses = await query({ "inputs": "teaching resources needed to introduce a lesson on  " + indicatorValue });
+    displayResponse(botResponses, 'resources');
+} else {
+    console.log('Selected option: Other');
+  
+}
+loader.style.display = 'none';
+});
+}
+
+// Function to display bot response in the specified textarea
+function displayResponse(botResponses, textareaId) {
+const responseTextarea = document.getElementById(textareaId);
+if (responseTextarea) {
+responseTextarea.value = ''; // Clear previous content if needed
+
+botResponses.forEach(botResponse => {
+    responseTextarea.value += botResponse.generated_text + '\n';
+});
+}
+}
+
+
+
+
+
+
+<!-- 
+// Function to retrieve and display notes based on the selected week
+document.getElementById("weekSelect").addEventListener("change", function () {
+    const selectedWeek = this.value;
+    retrieveNotes(selectedWeek);
 });
 
-// Load the lessons when the script is run
-loadLessons();
+function retrieveNotes(selectedWeek) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            const notes = JSON.parse(this.responseText);
+            // Populate the retrieved notes in the respective input fields
+            document.getElementsByName("duration")[0].value = notes.duration;
+            document.getElementsByName("strand")[0].value = notes.strand;
+            document.getElementsByName("class_size")[0].value = notes.class_size;
+            document.getElementsByName("sub_strand")[0].value = notes.sub_strand;
+            document.getElementsByName("content_standard")[0].value = notes.content_standard;
+            document.getElementsByName("indicator")[0].value = notes.indicator;
+            document.getElementsByName("lesson")[0].value = notes.lesson;
+            // Add similar lines for other fields as necessary
+        }
+    };
+    xhr.open("GET", `retrieve_notes.php?week=${selectedWeek}`, true);
+    xhr.send();
+}
+ -->
+
+
